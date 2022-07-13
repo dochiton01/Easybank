86ca3def76f3dc99b7590738eb633403b26c4a03
dochiton01 <98357012+dochiton01@users.noreply.github.com>
2022-07-13 12:25:24 -0400

    final commit

---
+++ app/js/script.js
@@ -0,0 +1,24 @@
+const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+header.addEventListener("click", function () {
+  console.log("open hamberger");
+  if (header.classList.contains("open")) {
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeElems.forEach(function (element) {
+      element.classList.add("fade-out");
+      element.classList.remove("fade-in");
+    });
+  } else {
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
+  }
+});
